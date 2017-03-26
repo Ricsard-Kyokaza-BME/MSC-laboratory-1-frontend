a2129954355f07190c45369d04398a2d06f2b145
Richard Balog <balog.richard.mail@gmail.com>
2017-03-26 15:27:30 +0200

    New dashboard ui

--- app/app.component.ts
+++ app/app.component.ts
@@ -4,16 +4,6 @@ import {SessionService} from "./auth/session.service";
 @Component({
     selector: 'my-app',
     template: `
-      <!--<nav class="blue-grey">-->
-        <!--<div class="nav-wrapper">-->
-        <!--<span style="padding: 0 15px; font-size: 20px;"><a routerLink="/" routerLinkActive="active">Agile Tool</a></span>-->
-          <!--<ul id="nav-mobile" class="right hide-on-med-and-down">-->
-            <!--<li><a routerLink="/backlog-item/create" routerLinkActive="active">Create Backlog Item</a></li>-->
-            <!--<li><a routerLink="/" routerLinkActive="active">Dashboard</a></li>-->
-            <!--<li><a href="/logout" routerLinkActive="active">Logout</a></li>-->
-          <!--</ul>-->
-        <!--</div>-->
-      <!--</nav>-->
       <nav id="menu" class="navbar navbar-toggleable-sm navbar-inverse">
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarToggler" aria-controls="navbarToggler" aria-expanded="false" aria-label="Toggle navigation">
           <span class="navbar-toggler-icon"></span>
