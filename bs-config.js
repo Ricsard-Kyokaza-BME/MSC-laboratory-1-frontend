c880679937948a1d995517246517eb67da495080
Richard Balog <balog.richard.mail@gmail.com>
2017-05-01 11:01:22 +0200

    Small css fix for prod build, lite-server verbose log turned off

--- bs-config.js
+++ bs-config.js
@@ -1,9 +1,3 @@
 module.exports = {
-  open: false,
-  server: {
-    middleware: {
-      // overrides the second middleware default with new settings
-      1: require('connect-history-api-fallback')({index: '/index.html', verbose: true})
-    }
-  }
+  open: false
 };
